94beb2cec5066fe893d2f0df12ac02c25f9014dc
jensanf <jensanf@gmail.com>
2023-08-13 23:59:30 +0800

    Get a current time from web

--- WiESPlayer.ino
+++ WiESPlayer.ino
@@ -10,6 +10,7 @@
 #include "SD.h"
 #include "FS.h"
 #include "RTClib.h"
+#include <WiFiClientSecure.h>
 
 // Digital I/O used
 #define SD_CS          5
@@ -47,6 +48,7 @@ int currentTrack = 0;
 const char* AP_SSID = "ESP32";
 const char* AP_PASSWORD = "12345678";
 const char* HOSTNAME = "esp32";
+String web_datetime; 
 
 // Global variables
 int currentVolume = 5; // Starting volume of audio player
@@ -436,6 +438,7 @@ void configMainPage() {
     String main_page = FPSTR(main_page_start);   // start the HTML
     main_page += R"rawliteral(<label id="volumeLabel1">Volume:)rawliteral" + String(currentVolume) + "</label> <br/> ";
     main_page += R"rawliteral(<input type="range" min="0" max="21" value=")rawliteral" + String(currentVolume) + R"rawliteral(" id="volumeSlider1"><br/>)rawliteral"; 
+    main_page += "<p> Web Time: " +   fetchTimeFromWorldTimeAPI() + "</p>";
     if (flagModeAlarm) {
       main_page += "<p> Alarm is set: " + String(alarmHour) + ":" + fineStrMinute(alarmMinute) + "</p>";
     }
@@ -702,3 +705,34 @@ String fineStrMinute(int minute){
   fineMinute += String(minute); 
   return fineMinute;
 }
+
+String fetchTimeFromWorldTimeAPI() {
+  WiFiClientSecure client;
+  client.setInsecure();
+  HTTPClient https;
+
+    // Specify to trust any certificate for this connection (not secure for production!)
+    https.begin(client, "https://time.is/clock");
+
+    int httpCode = https.GET();
+
+    if (httpCode > 0) {
+      String payload = https.getString();
+      int titleStart = payload.indexOf("<title>") + 7;
+      int titleEnd = payload.indexOf("</title>");
+
+      if(titleStart != -1 && titleEnd != -1) {
+        String titleContent = payload.substring(titleStart, titleEnd);
+        Serial.println(titleContent);
+        https.end();
+        return titleContent;
+      }
+    } else {
+      Serial.printf("[HTTP] GET... failed, error: %s\n", https.errorToString(httpCode).c_str());
+      https.end();
+      return https.errorToString(httpCode).c_str();
+    }
+
+    https.end();
+    return "FAIL";
+  }
